ed634ea8e6ee50a6448479e13e5e024888584969
Kitozuri <nnguyenhoanggiahuy.71@gmail.com>
2024-12-07 19:12:37 +0700

    111

--- 444/ViewController.swift
+++ 444/ViewController.swift
@@ -8,12 +8,104 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var results : UILabel!
+    //
+    var firstNumber: Double = 0
+    var currentOperation:String = ""
+    var isInputCompleted = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
+//    // pt nhập số vào màn hình
+//    func addNumber(_ number: String) {
+////        if isInputCompleted || results.text == "0" {
+////            results.text = number
+////            isInputCompleted = false
+////        }else{
+////            results.text! += number
+////        }
+//    }
+//    
+    
+    
+    
+    
+    
+//    
+//    @IBAction func AC(_ sender: Any) {
+//    }
+//    
+//    
+//    @IBAction func phan(_ sender: Any) {
+//    }
+//    
+//    @IBAction func phantram(_ sender: Any) {
+//    }
+//    
+//    @IBAction func chia(_ sender: Any) {
+//    }
+//    
+//    @IBAction func nhan(_ sender: Any) {
+//    }
+//    
+//    @IBAction func tru(_ sender: Any) {
+//    }
+//    
+//    @IBAction func cong(_ sender: Any) {
+//    }
+//    
+//    @IBAction func bang(_ sender: Any) {
+//    }
+//    
+//    @IBAction func so7(_ sender: Any) {
+//        addNumber("7")
+//    }
+//    
+//    @IBAction func so8(_ sender: Any) {
+//        addNumber("8")
+//    }
+//    
+//    
+//    @IBAction func so9(_ sender: Any) {
+//        addNumber("9")
+//    }
+//    
+//    @IBAction func so4(_ sender: Any) {
+//        addNumber("4")
+//    }
+//    
+//    @IBAction func so5(_ sender: Any) {
+//        addNumber("5")
+//    }
+//    
+//    @IBAction func so6(_ sender: Any) {
+//        addNumber("6")
+//    }
+//    
+//    @IBAction func so1(_ sender: Any) {
+//        addNumber("1")
+//    }
+//    
+//    @IBAction func so2(_ sender: Any) {
+//        addNumber("2")
+//    }
+//    
+//    @IBAction func so3(_ sender: Any) {
+//        
+//        addNumber("3")
+//    }
+//    @IBAction func so0(_ sender: Any) {
+//        addNumber("0")
+//    }
+//    
+//    @IBAction func dauphay(_ sender: Any) {
+//    }
+//    
+//}
+//
+//
+//
